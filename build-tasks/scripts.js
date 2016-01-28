8e6290b7b169445c2cd12b94638fd88fb8469560
jh3y <jh3y@users.noreply.github.com>
2016-01-28 19:33:21 +0000

    Refactor structure of boilerplate and add dependency update script

---
+++ build-tasks/scripts.js
@@ -0,0 +1,41 @@
+var gulp      = require('gulp'),
+  gConfig     = require('../gulp-config'),
+  utils       = require('./utils'),
+  opts        = gConfig.pluginOpts,
+  env         = utils.getEnv(),
+  src         = gConfig.paths.sources,
+  dest        = gConfig.paths.destinations,
+  plugins     = require('gulp-load-plugins')(opts.load),
+  /* scripts:lint */
+  lint = function() {
+    return gulp.src(src.scripts)
+      .pipe(plugins.coffeelint())
+      .pipe(plugins.coffeelint.reporter());
+  },
+  /* scripts:compile */
+  compile = function() {
+    return gulp.src(src.scripts)
+      .pipe(plugins.plumber())
+      .pipe(plugins.coffee(opts.coffee))
+      .pipe(env.mapped ? gulp.dest(dest.js): plugins.gUtil.noop())
+      .pipe(env.mapped ? plugins.sourcemaps.init(): plugins.gUtil.noop())
+      .pipe(plugins.concat(gConfig.pkg.name + '.js'))
+      .pipe(plugins.wrap(opts.wrap))
+      .pipe(env.stat ? plugins.size(opts.gSize): plugins.gUtil.noop())
+      .pipe(env.deploy ? plugins.gUtil.noop(): gulp.dest(env.dist ? dest.dist: dest.js))
+      .pipe(plugins.uglify())
+      .pipe(plugins.rename(opts.rename))
+      .pipe(env.mapped ? plugins.sourcemaps.write('./'): plugins.gUtil.noop())
+      .pipe(env.stat ? plugins.size(opts.gSize): plugins.gUtil.noop())
+      .pipe(gulp.dest(env.dist ? dest.dist: dest.js));
+  },
+  /* scripts:watch */
+  watch = function() {
+    gulp.watch(src.scripts, ['scripts:compile']);
+  };
+
+module.exports = {
+  lint   : lint,
+  compile: compile,
+  watch  : watch
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,15 @@
-var gulp      = require('gulp'),
-  gConfig     = require('./gulp-config'),
-  browserSync = require('browser-sync'),
-  opts        = gConfig.pluginOpts,
-  plugins     = require('gulp-load-plugins')(opts.load),
-  isDist      = (plugins.gUtil.env.dist)    ? true: false,
-  isDev       = (plugins.gUtil.env.dev)     ? true: false,
-  isDeploy    = (plugins.gUtil.env.deploy)  ? true: false,
-  isMapped    = (plugins.gUtil.env.map)     ? true: false,
-  isStat      = (plugins.gUtil.env.stat)    ? true: false,
-  src         = gConfig.paths.sources,
-  dest        = gConfig.paths.destinations;
+var gulp  = require('gulp'),
+  gutil   = require('gulp-util'),
+  server  = require('./build-tasks/server'),
+  scripts = require('./build-tasks/scripts'),
+  styles  = require('./build-tasks/styles'),
+  markup  = require('./build-tasks/markup'),
+  deploy  = require('./build-tasks/deploy');
 
 /*
   serve; creates local static livereload server using browser-sync.
 */
-gulp.task('serve', ['compile'], function(event) {
-  browserSync(opts.browserSync);
-  return gulp.watch(src.overwatch).on('change', function(file) {
-    if (file.path.indexOf('.css') === -1) browserSync.reload();
-  });
-});
+gulp.task('serve', ['compile'], server.start);
 
 /*
   scripts:compile/scripts:watch
@@ -27,30 +17,9 @@ gulp.task('serve', ['compile'], function(event) {
   watch for changes to scriptsScript files then compile app JavaScript file
   from source, concatenating and uglifying content and publishing output based on env flag. For example, if we want sourcemaps we can output our individual JS files and the sourcemap for them to the desired directory by using the --map flag.
 */
-gulp.task('scripts:lint', function() {
-  return gulp.src(src.scripts)
-    .pipe(plugins.coffeelint())
-    .pipe(plugins.coffeelint.reporter());
-});
-gulp.task('scripts:compile', ['scripts:lint'], function(event) {
-  return gulp.src(src.scripts)
-    .pipe(plugins.plumber())
-    .pipe(plugins.coffee(opts.coffee))
-    .pipe(isMapped ? gulp.dest(dest.js): plugins.gUtil.noop())
-    .pipe(isMapped ? plugins.sourcemaps.init(): plugins.gUtil.noop())
-    .pipe(plugins.concat(gConfig.pkg.name + '.js'))
-    .pipe(plugins.wrap(opts.wrap))
-    .pipe(isStat ? plugins.size(opts.gSize): plugins.gUtil.noop())
-    .pipe(isDeploy ? plugins.gUtil.noop(): gulp.dest(isDist ? dest.dist: dest.js))
-    .pipe(plugins.uglify())
-    .pipe(plugins.rename(opts.rename))
-    .pipe(isMapped ? plugins.sourcemaps.write('./'): plugins.gUtil.noop())
-    .pipe(isStat ? plugins.size(opts.gSize): plugins.gUtil.noop())
-    .pipe(gulp.dest(isDist ? dest.dist: dest.js));
-});
-gulp.task('scripts:watch', function(event) {
-  gulp.watch(src.scripts, ['scripts:compile']);
-});
+gulp.task('scripts:lint', scripts.lint);
+gulp.task('scripts:compile', ['scripts:lint'], scripts.compile);
+gulp.task('scripts:watch', scripts.watch);
 
 /*
   styles:compile/styles:watch
@@ -58,28 +27,9 @@ gulp.task('scripts:watch', function(event) {
   watch for changes to styles files then compile stylesheet from source
   auto prefixing content and generating output based on env flag.
 */
-gulp.task('styles:lint', function() {
-  return gulp.src(src.styles)
-    .pipe(plugins.stylint(opts.stylint))
-    .pipe(plugins.stylint.reporter());
-});
-gulp.task('styles:compile', ['styles:lint'], function(event) {
-  return gulp.src(src.styles)
-    .pipe(plugins.plumber())
-    .pipe(plugins.concat(gConfig.pkg.name + '.stylus'))
-    .pipe(plugins.stylus())
-    .pipe(plugins.prefix(opts.prefix))
-    .pipe(isStat ? plugins.size(opts.gSize): plugins.gUtil.noop())
-    .pipe(isDeploy ? plugins.gUtil.noop(): gulp.dest(isDist ? dest.dist: dest.css))
-    .pipe(plugins.minify())
-    .pipe(plugins.rename(opts.rename))
-    .pipe(isStat ? plugins.size(opts.gSize): plugins.gUtil.noop())
-    .pipe(gulp.dest(isDist ? dest.dist: dest.css))
-    .pipe(browserSync.stream());
-});
-gulp.task('styles:watch', function(event) {
-  gulp.watch(src.styles, ['styles:compile']);
-});
+gulp.task('styles:lint', styles.lint);
+gulp.task('styles:compile', ['styles:lint'], styles.compile);
+gulp.task('styles:watch', styles.watch);
 
 /*
   markup:compile/markup:watch
@@ -87,25 +37,11 @@ gulp.task('styles:watch', function(event) {
   watch for all markup file changes then compile
   page document files.
 */
-gulp.task('markup:lint', function() {
-  return gulp.src(src.markup)
-    .pipe(plugins.jadelint());
-});
-gulp.task('markup:compile', function() {
-  return gulp.src(src.docs)
-    .pipe(plugins.plumber())
-    .pipe(isDeploy ? plugins.jade(): plugins.jade(opts.jade))
-    .pipe(gulp.dest(dest.html));
-});
-gulp.task('markup:watch', function(event){
-  gulp.watch(src.markup, ['markup:compile']);
-});
+gulp.task('markup:lint', markup.lint);
+gulp.task('markup:compile', markup.compile);
+gulp.task('markup:watch', markup.watch);
 
-gulp.task('deploy', ['compile'], function(event) {
-  isDeploy = true;
-  return gulp.src(src.overwatch)
-    .pipe(plugins.deploy());
-});
+gulp.task('deploy', ['compile'], deploy.run);
 
 gulp.task('compile', [
   'markup:compile',
@@ -119,7 +55,7 @@ gulp.task('watch', [
   'scripts:watch'
 ]);
 
-var defaultTasks = isDeploy ? [
+var defaultTasks = ((gutil.env.deploy) ? true: false) ? [
   'deploy'
 ]:[
   'serve',
