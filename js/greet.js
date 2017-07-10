bbc65b0756f8b73f0346080e6417304c8c1d8fde
jh3y <jh3y@users.noreply.github.com>
2017-07-11 00:01:11 +0100

    Update 2017-07-10T23:01:09.053Z

--- js/greet.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var msg = 'gulper';
-var greet = function greet(txt) {
-  return 'Hello ' + txt;
-};
-greet(msg);
--- js/sum.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var sum = function sum(a, b) {
-  return a + b;
-};
-sum(1, 2);
