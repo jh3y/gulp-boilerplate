bf7a3506e4c0cfb3ccf471206ee6c91f20dd1782
jh3y <jh3y@users.noreply.github.com>
2017-10-02 21:35:37 +0100

    Resolve #15 by adding metadata to tasks

--- build-tasks/BOILERPLATE.js
+++ build-tasks/BOILERPLATE.js
@@ -1,20 +1,33 @@
-var gulp      = require('gulp'),
-  gConfig     = require('../gulp-config'),
-  utils       = require('./utils'),
-  opts        = gConfig.pluginOpts,
-  env         = utils.getEnv(),
-  src         = gConfig.paths.sources,
-  dest        = gConfig.paths.destinations,
-  plugins     = require('gulp-load-plugins')(opts.load),
-  /* TODO: populate linting */
-  lint = function() {},
-  /* TODO: populate compilation task */
-  compile = function() {},
-  /* TODO: populate watching task */
-  watch = function() {};
+var gulp  = require('gulp'),
+  gConfig  = require('../gulp-config'),
+  utils   = require('./utils'),
+  opts    = gConfig.pluginOpts,
+  env     = utils.getEnv(),
+  src     = gConfig.paths.sources,
+  dest    = gConfig.paths.destinations,
+  plugins = require('gulp-load-plugins')(opts.load);
+
+/* TODO: populate linting */
+var lint = function() {}
+/* TODO: lint description */
+lint.description = ``
+/* TODO: lint flags */
+lint.flags = {}
+/* TODO: populate compilation task */
+var compile = function() {}
+/* TODO: compile description */
+compile.description = ``
+/* TODO: compile flags */
+compile.flags = {}
+/* TODO: populate watching task */
+var watch = function() {}
+/* TODO: watch description */
+watch.description = ``
+/* TODO: watch flags */
+watch.flags = {}
 
 module.exports = {
   lint   : lint,
   compile: compile,
-  watch  : watch
-};
+  watch  : watch,
+}
--- build-tasks/markup.js
+++ build-tasks/markup.js
@@ -1,32 +1,40 @@
-var gulp      = require('gulp'),
-  gConfig     = require('../gulp-config'),
-  keys        = require('../gulp-keys'),
-  utils       = require('./utils'),
-  opts        = gConfig.pluginOpts,
-  env         = utils.getEnv(),
-  src         = gConfig.paths.sources,
-  dest        = gConfig.paths.destinations,
-  plugins     = require('gulp-load-plugins')(opts.load),
-  /* markup:lint */
-  lint = function() {
-    return gulp.src(src.markup)
-      .pipe(plugins.pugLint());
-  },
-  /* markup:compile */
-  compile = function() {
-    if (env.deploy && opts.pug.pretty) opts.pug.pretty = false;
-    return gulp.src(src.docs)
-      .pipe(plugins.plumber())
-      .pipe(plugins.pug(opts.pug))
-      .pipe(gulp.dest(dest.html));
-  },
-  /* markup:watch */
-  watch = function() {
-    gulp.watch(src.markup, [keys.compile_markup]);
-  };
+var gulp  = require('gulp'),
+  gConfig  = require('../gulp-config'),
+  keys    = require('../gulp-keys'),
+  utils   = require('./utils'),
+  opts    = gConfig.pluginOpts,
+  env     = utils.getEnv(),
+  src     = gConfig.paths.sources,
+  dest    = gConfig.paths.destinations,
+  plugins = require('gulp-load-plugins')(opts.load);
+
+/* markup:lint */
+var lint = function() {
+  return gulp.src(src.markup)
+    .pipe(plugins.pugLint())
+}
+lint.description = `lint markup source(${src.markup}) using pug-lint`
+
+/* markup:compile */
+var compile = function() {
+  if (env.deploy && opts.pug.pretty) opts.pug.pretty = false
+  return gulp.src(src.docs)
+    .pipe(plugins.plumber())
+    .pipe(plugins.pug(opts.pug))
+    .pipe(gulp.dest(dest.html))
+}
+compile.description = `compile markup source(${src.markup}) using pug`
+compile.flags = {
+  '--deploy': 'Turns off pretty option in pug and removes whitespace from output'
+}
+/* markup:watch */
+var watch = function() {
+  gulp.watch(src.markup, [keys.compile_markup])
+};
+watch.description = `watch for changes in markup source(${src.markup}) and compile on change`
 
 module.exports = {
   lint   : lint,
   compile: compile,
-  watch  : watch
-};
+  watch  : watch,
+}
